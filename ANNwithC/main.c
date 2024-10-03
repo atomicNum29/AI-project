d8a81748284943963a234a17b7484332c0c52f26
atomicNum29 <atomicw63.546@gmail.com>
2024-10-03 22:28:52 +0900

    행렬곱 함수 작성

--- ANNwithC/main.c
+++ ANNwithC/main.c
@@ -26,7 +26,23 @@ void matrix_multiplication(matrix res, matrix A, matrix B)
 {
     if (res.row < A.row || res.col < B.col)
         perror("MAT MUL: 'res' is too small");
+    if (A.col != B.row)
+        perror("MAT MUL: matrix size error\n\tA.col != B.row");
 
+    for (size_t i = 0; i < A.row; i++)
+    {
+        for (size_t ii = 0; ii < B.col; ii++)
+        {
+            dtype tmp = 0;
+            for (size_t iii = 0; iii < A.col; iii++)
+            {
+                tmp += A.data[i][iii] * B.data[iii][ii];
+            }
+            res.data[i][ii] = tmp;
+        }
+    }
+    
+    return;
 }
 
 dtype sigmoid(dtype x)
